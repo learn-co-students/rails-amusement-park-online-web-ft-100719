5e21d6cfb515e8fabd846250d0aaab2111d5edff
Cristal Alatorre Perez <cristalalatorreperez@albert.home>
2020-01-10 18:03:06 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new'
   post '/users/new', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
 
 
 end
