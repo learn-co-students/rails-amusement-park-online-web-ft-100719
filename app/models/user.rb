00feb593ea3f544ff1b0e2b1ba043fba7b1a496c
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-13 18:19:29 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+    has_secure_password
+    has_many :rides 
+    has_many :attractions, through: :rides
+
+    def mood 
+        if self.admin == true 
+            " "
+        else 
+            self.happiness > self.nausea ? 'happy' : 'sad'
+        end 
+    end 
 end
